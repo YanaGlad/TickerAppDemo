0d2a7b61aa9363639f97ae649ad0eb3fa72a8113
YanaGlad <olesya_leader@mail.ru>
2021-03-20 17:27:57 +0300

    adapter

--- app/src/main/java/com/example/tickersapp12/TickerAdapter.java
+++ app/src/main/java/com/example/tickersapp12/TickerAdapter.java
@@ -1,13 +1,25 @@
 package com.example.tickersapp12;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tickersapp12.Support.TickerInfo;
+
+import java.util.List;
+
 public class TickerAdapter extends RecyclerView.Adapter {
 
+    private List<TickerInfo>tickerInfos;
+    private Context context;
 
+    TickerAdapter(List<TickerInfo>tickerInfos, Context context){
+        this.tickerInfos = tickerInfos;
+        this.context = context;
+        
+    }
 
     @NonNull
     @Override
--- app/src/main/java/com/example/tickersapp12/TickerViewHolder.java
+++ app/src/main/java/com/example/tickersapp12/TickerViewHolder.java
@@ -1,4 +1,48 @@
 package com.example.tickersapp12;
 
-public class TickerViewHolder {
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynasaapp.R;
+
+public class TickerViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView ticker, company, price, priceChange;
+    private Button fav, buy, sell;
+
+    public TickerViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        ticker = itemView.findViewById(R.id.nameTicker);
+        company = itemView.findViewById(R.id.companyTicker);
+        price = itemView.findViewById(R.id.priceTicker);
+        priceChange = itemView.findViewById(R.id.changeTicker);
+        buy = itemView.findViewById(R.id.buy);
+        sell = itemView.findViewById(R.id.sell);
+        fav = itemView.findViewById(R.id.fav);
+
+    }
+    void sell(View view){
+
+    }
+
+    void buy(View view){
+
+    }
+
+    void addToFavs(View view){
+
+    }
+
+    void setDescription(Context context, String ticker, String company, String price, String priceChange) {
+        this.ticker.setText(ticker);
+        this.company.setText(company);
+        this.price.setText(price);
+        this.priceChange.setText(priceChange);
+    }
 }
